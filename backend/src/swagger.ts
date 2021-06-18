33b74ad2016a63c74ecf69b79dcb8fd653626c3c
LucasGSilva21 <lucasgsilva2102@gmail.com>
2021-06-18 18:51:14 -0300

    change title in swagger.ts

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -1,22 +1,17 @@
-import { Logger, INestApplication } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-export async function setupSwagger(
-  app: INestApplication,
-  isServerless = false,
-): Promise<any> {
-  const logger: Logger = new Logger('Swagger');
+export async function setupSwagger(app: INestApplication): Promise<any> {
   const swaggerEndpoint = '/api-docs';
 
   const options = new DocumentBuilder()
-    .setTitle('Lutien API')
+    .setTitle('Serverless Challenge API')
     .setDescription('Lutien API documentation')
     .setVersion('0.0.1')
     .addBearerAuth()
     .setBasePath('/api/')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup(swaggerEndpoint, app, document);
 
-  logger.log(`Added swagger on endpoint ${swaggerEndpoint}`);
+  SwaggerModule.setup(swaggerEndpoint, app, document);
 }
