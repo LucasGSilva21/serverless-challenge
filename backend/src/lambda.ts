38a33c96cbec58c2027019ccf0f079975a19c47f
LucasGSilva21 <lucasgsilva2102@gmail.com>
2021-06-19 16:29:26 -0300

    config serverless

---
+++ backend/src/lambda.ts
@@ -0,0 +1,46 @@
+// lambda.ts
+import { Handler, Context } from 'aws-lambda';
+import { Server } from 'http';
+import { createServer, proxy } from 'aws-serverless-express';
+import { eventContext } from 'aws-serverless-express/middleware';
+
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app/app.module';
+
+import express = require('express');
+import { setupSwagger } from './swagger';
+
+// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
+// due to a compressed response (e.g. gzip) which has not been handled correctly
+// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
+// binaryMimeTypes below
+const binaryMimeTypes: string[] = [];
+
+//const isProduction = process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'dev';
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+
+    setupSwagger(nestApp);
+    nestApp.enableCors();
+
+    nestApp.use(eventContext());
+    await nestApp.init();
+
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+  }
+  return cachedServer;
+}
+
+export const handler: Handler = async (event: any, context: Context) => {
+  cachedServer = await bootstrapServer();
+
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -1,12 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-export async function setupSwagger(app: INestApplication): Promise<any> {
+export async function setupSwagger(
+  app: INestApplication
+): Promise<any> {
   const swaggerEndpoint = '/api-docs';
 
   const options = new DocumentBuilder()
     .setTitle('Serverless Challenge API')
-    .setDescription('Lutien API documentation')
+    .setDescription('Serverless Challenge API documentation')
     .setVersion('0.0.1')
     .setBasePath('/api/')
     .build();
